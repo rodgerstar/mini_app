354223e86211b71cac2360000ffe318d4435a896
rodgers <rodgerstarrodgers@gmail.com>
2024-11-16 00:56:37 +0300

    first commit

---
+++ sacco/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    dob = models.DateField()
+    gender = models.CharField(max_length=10)
+    weight = models.IntegerField(default=0)
+
+
